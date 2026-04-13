eb979a1ab3a1749b43a18791514af4b82946f6fe
Mashrfee Aryan <aryanmashrafee07@gmail.com>
2026-04-13 17:39:28 -0500

    Added test cases to print out the matrix to see if it's working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-
+#include "matrix/matrix.h"
 
 int main(){
     std:: cout << "Neural Network in C+++ " << std::endl;
-    return 0
+
+
+    //Test if matrix is working:
+    //row=2
+    //col=2
+    Matrix m(2,2);
+    //fills matrix with numbers between -1.0 to 1.0
+    m.randomize();
+
+    //print out the four values
+    std::cout << m(0,0) << " " << m(0,1) << std::endl;
+    std::cout << m(1,0) << " " << m(1,1) << std::endl;
+    
+    
 }
