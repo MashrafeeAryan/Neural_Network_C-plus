685a305552ad0403962d3627d003cf34f41299ae
Mashrfee Aryan <aryanmashrafee07@gmail.com>
2026-04-13 16:11:12 -0500

    Finished header of matrix

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -16,6 +16,17 @@ public:
     //Notes to myself
     //operator() overloads a function
         // same function name but different behavior depending on inputs
-    double&
 
-}
+    //Since matrix is 1D, to access the values you need to do data[1* m.cols +2] to access index
+    //However, this does the same without the indexing manualy
+    
+    //Gives reference for functions to write it
+    double& oeperator()(int r, int c);
+
+    //Only used for reading. That's why using const. Can't be modified
+    double operator()(int r, int c); const;
+
+    // declaring function; It will fill matrix with random numbers
+    void randomize();
+
+};
